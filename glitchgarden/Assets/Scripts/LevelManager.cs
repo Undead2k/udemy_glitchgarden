ed3dba78ca6a3d16276ce2e0d46be4e248d936f4
Undead2k <addie2k3@hotmail.com>
2016-03-06 13:20:15 +0000

    First commit!
    Actually get things committed!

---
+++ glitchgarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start () {
+		if (autoLoadNextLevelAfter == 0){
+		
+		}
+
+		else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+
+		}
+	}
+
+	public void LoadLevel(string name){
+		print("Level load requested for: "+name);
+		Application.LoadLevel(name);
+	
+	}
+	
+	public void QuitRequest(){
+		print("quit request");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+}
---
+++ glitchgarden/Assets/Scripts/action_fade.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class action_fade : MonoBehaviour {
+
+	public float fadeInTime;
+
+	private Image fadePanel;
+	private Color currentColour = Color.black;
+
+
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad < fadeInTime){
+			//fadein
+		} else {
+			gameObject.SetActive (false);
+		}
+
+	}
+}
