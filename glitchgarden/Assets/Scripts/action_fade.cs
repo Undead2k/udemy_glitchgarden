4b90046c295d3fdd99393f783482942263717003
Undead2k <addie2k3@hotmail.com>
2016-03-07 23:10:07 +0000

    Fade in and fade out
    Working on fading between scenes, or at least making it look liek it.
    Also added a delay.
    
    Next we are working on player pref

--- glitchgarden/Assets/Scripts/action_fade.cs
+++ glitchgarden/Assets/Scripts/action_fade.cs
@@ -4,25 +4,83 @@ using System.Collections;
 
 public class action_fade : MonoBehaviour {
 
-	public float fadeInTime;
+	public float fade;
 
 	private Image fadePanel;
 	private Color currentColour = Color.black;
+	private float alphaStart;
 
+	public float delay;
 
+	public bool b_fadeOut;
+	public bool b_fadeIn;
 
 	// Use this for initialization
 	void Start () {
+		//gets access to componet features so we can manipulate things later
 		fadePanel = GetComponent<Image>();
+
+		//sets the alpha at start
+		if (b_fadeOut){
+			alphaStart = 1.0f;
+			currentColour.a = alphaStart;
+		} 
+
+		if (b_fadeIn) {
+			alphaStart = 0.0f;
+			currentColour.a = alphaStart;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.timeSinceLevelLoad < fadeInTime){
-			//fadein
+
+		if (b_fadeOut){
+			fadeOut();
+		} 
+
+		if(b_fadeIn){
+
+			//if 0 delay, it just fades in as normal. 
+			//add a delay and it will pause before fading in.
+			StartCoroutine(startDelay());
+
+			//fadeIn();
+		}
+
+
+		if (b_fadeOut && b_fadeIn){
+			Debug.Log("This will do nothing. Set only Fade in or Fade out!");
+		} 
+
+	}
+
+	void fadeOut(){
+		if (Time.timeSinceLevelLoad < fade){
+			float alphaChange = Time.deltaTime / fade;
+			currentColour.a -= alphaChange;
+			fadePanel.color = currentColour;
 		} else {
 			gameObject.SetActive (false);
 		}
-
 	}
+	
+	void fadeIn(){
+		//if (Time.timeSinceLevelLoad < fade){
+		float alphaChange = Time.deltaTime / fade;
+		currentColour.a += alphaChange;
+		fadePanel.color = currentColour;
+		//} 
+	}
+
+
+	IEnumerator startDelay(){
+
+		yield return new WaitForSeconds(delay);
+
+		fadeIn ();
+	}
+
+
+
 }
