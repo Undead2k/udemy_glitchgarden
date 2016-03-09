0d752f365e300c35c655cf60991ce4b47c53d2e8
Undead2k <addie2k3@hotmail.com>
2016-03-09 17:25:46 +0000

    PlayerPref work part1
    Setting up the basic framework for playerpref and our wrapper. Need to
    do last challenge which is to do the difficulty pref on our own!

---
+++ glitchgarden/Assets/Scripts/PlayerPrefManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefManager : MonoBehaviour {
+
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
+		//level_unlocked_1 
+
+
+	public static void SetMasterVolume (float volume) {
+		if (volume > 0f && volume < 1f) {
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		} else {
+			Debug.LogError("Master volume out of range");
+		}
+	}
+
+	public static float GetMasterVolume(){
+		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
+	}
+
+
+	public static void SetLevelUnlock(int level){
+		if(level <= Application.levelCount -1){
+			PlayerPrefs.SetInt (LEVEL_KEY + level.ToString(), 1); // use 1 for true, as we cant use BOOL
+		} else {
+			Debug.LogError("Trying to unlock level not in build order");
+		}
+	}
+
+	public static bool IsLevelUnlocked (int level) {
+		int levelValue = PlayerPrefs.GetInt (LEVEL_KEY + level.ToString ());
+		bool isLevelUnlocked = (levelValue == 1);
+
+		if (level <= Application.levelCount - 1) {
+			return isLevelUnlocked;
+		} else {
+			Debug.LogError("Trying to query level not in build order");
+			return false;
+		} 
+	}
+
+}
---
+++ glitchgarden/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		print (PlayerPrefManager.GetMasterVolume ());
+		PlayerPrefManager.SetMasterVolume (0.2f);
+		print (PlayerPrefManager.GetMasterVolume ());
+
+		print (PlayerPrefManager.IsLevelUnlocked (3));
+		PlayerPrefManager.SetLevelUnlock (3);
+		print (PlayerPrefManager.IsLevelUnlocked (3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
